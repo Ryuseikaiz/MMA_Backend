c37bd119f88154e9ceba9d3c442d8a4da53b990b
Ryuseikai <kthkun@gmail.com>
2025-07-16 19:19:24 +0700

    quên thêm location

--- models/Product.js
+++ models/Product.js
@@ -5,6 +5,7 @@ const ProductSchema = new mongoose.Schema({
   description: { type: String, required: true },
   price: { type: Number, required: true },
   imageUrl: { type: String, required: false, default: '' },
+  location: { type: String, required: true }, 
 }, { timestamps: true });
 
 module.exports = mongoose.model('Product', ProductSchema);
